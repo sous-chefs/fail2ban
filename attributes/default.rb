1556dab0d831545861dc8cdc4dc90ef3cf481ebd
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-10-10 11:10:05 +0200

    Change double to single quotes

--- attributes/default.rb
+++ attributes/default.rb
@@ -47,10 +47,10 @@ end
 
 default['fail2ban']['services'] = {
   'ssh' => {
-        "enabled" => "true",
-        "port" => "ssh",
-        "filter" => "sshd",
-        "logpath" => node['fail2ban']['auth_log'],
-        "maxretry" => "6"
+        'enabled' => 'true',
+        'port' => 'ssh',
+        'filter' => 'sshd',
+        'logpath' => node['fail2ban']['auth_log'],
+        'maxretry' => '6'
      }
 }
