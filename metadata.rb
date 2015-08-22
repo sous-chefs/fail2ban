5fb27c00657fc49ba9c8c5dd4b95aaf55c877f7a
Tim Smith <tim@cozy.co>
2015-08-22 10:49:03 -0700

    Bump version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures fail2ban'
-version '2.3.0'
+version '2.3.1'
 
 recipe 'default', 'Installs and configures fail2ban'
 
