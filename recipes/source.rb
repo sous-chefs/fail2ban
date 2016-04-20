3347852a8103e2199eb3c6f5f44ee71946a60f24
salelkar-ibm <salelkar@us.ibm.com>
2016-04-20 16:28:02 +0530

    Update source.rb

--- recipes/source.rb
+++ recipes/source.rb
@@ -25,40 +25,6 @@ remote_file File.join(Chef::Config[:file_cache_path], "fail2ban-#{node['fail2ban
 end
 
 execute 'copy-config' do
-  command 'pwd && cp /var/chef/cache/fail2ban-0.9.3/files/redhat-initd /etc/init.d/fail2ban && chmod 755 /etc/init.d/fail2ban && chkconfig --add fail2ban && chkconfig fail2ban on'
+  command 'pwd && cp /var/chef/cache/fail2ban-#{node['fail2ban']['source']['version']}/files/redhat-initd /etc/init.d/fail2ban && chmod 755 /etc/init.d/fail2ban && chkconfig --add fail2ban && chkconfig fail2ban on'
 end
 
-node['fail2ban']['filters'].each do |name, options|
-  template "/etc/fail2ban/filter.d/#{name}.conf" do
-    source 'filter.conf.erb'
-    variables(failregex: [options['failregex']].flatten, ignoreregex: [options['ignoreregex']].flatten)
-    notifies :restart, 'service[fail2ban]'
-  end
-end
-
-template '/etc/fail2ban/fail2ban.conf' do
-  source 'fail2ban.conf.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :restart, 'service[fail2ban]'
-end
-
-template '/etc/fail2ban/jail.local' do
-  source 'jail.conf.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :restart, 'service[fail2ban]'
-end
-
-service 'fail2ban' do
-  supports [status: true, restart: true]
-  action [:enable, :start]
-
-  if (platform?('ubuntu') && node['platform_version'].to_f < 12.04) ||
-     (platform?('debian') && node['platform_version'].to_f < 7)
-    # status command returns non-0 value only since fail2ban 0.8.6-3 (Debian)
-    status_command "/etc/init.d/fail2ban status | grep -q 'is running'"
-  end
-end
