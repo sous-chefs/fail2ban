b8bcc698a217a47f8fcd48573d437c92c95addae
Jennifer Davis <iennae@gmail.com>
2016-07-19 13:22:01 -0700

    adding fixture cookbook

---
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -0,0 +1,7 @@
+name 'test'
+description 'Installs/Configures test'
+long_description 'Installs/Configures test'
+version '0.1.0'
+
+depends 'openssh'
+depends 'fail2ban'
---
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -0,0 +1,2 @@
+include_recipe 'openssh::default'
+include_recipe 'fail2ban::default'
