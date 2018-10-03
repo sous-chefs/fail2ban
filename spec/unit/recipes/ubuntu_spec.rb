46569f8132e02799666548fb7e19b8a838cbf126
Rob Chekaluk <rchekaluk@ppolitics.org>
2018-10-03 12:02:39 -0400

    Use Gem::Version for version comparison Ubuntu 18.04 provides fail2ban 0.10 Reference https://medium.com/little-programming-joys/how-to-compare-version-strings-in-ruby-ced0916a4b71
    Signed-off-by: Rob Chekaluk <rchekaluk@devandops.consulting>

--- spec/unit/recipes/ubuntu_spec.rb
+++ spec/unit/recipes/ubuntu_spec.rb
@@ -31,3 +31,25 @@ describe 'default recipe on Ubuntu 14.04' do
     expect(chef_run).to render_file('/etc/fail2ban/fail2ban.conf').with_content(/^loglevel = 3/)
   end
 end
+
+describe 'default recipe on Ubuntu 18.04' do
+  let(:chef_run) do
+    runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '18.04')
+    runner.node.normal['packages']['fail2ban'] = { version: '0.10.2-2', arch: 'all' }
+    runner.converge('fail2ban::default')
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+
+  it 'should template fail2ban.conf' do
+    expect(chef_run).to render_file('/etc/fail2ban/fail2ban.conf').with_content { |content|
+      expect(content).to match(/^loglevel = INFO/)
+      expect(content).to match(/^syslogsocket = auto/)
+      expect(content).to match(/^dbfile = \/var\/lib\/fail2ban\/fail2ban.sqlite3/)
+      expect(content).to match(/^dbpurgeage = 86400/)
+    }
+  end
+end
+
